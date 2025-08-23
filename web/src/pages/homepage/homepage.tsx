b3ef3dfe20f8b05da07b8425bbac401926167e43
Debora <debora.pintarelli@gmail.com>
2025-08-23 09:25:39 +0200

    update

--- web/src/pages/homepage/homepage.tsx
+++ web/src/pages/homepage/homepage.tsx
@@ -1,12 +1,28 @@
 import { Header } from "../components/header";
+import { Table } from "../components/table";
+import style from "./homepage.module.css";
 
 export const Homepage = () => {
   return (
-    <div className="home-container">
+    <div className={style["home-container"]}>
         <Header />
-        <div className="content-container">
-          <h1>Benvenuto nella tua area personale!</h1>
+        <div className={style["content-container"]}>
+          <div className={style["filters-container"]}>
+            <div className={style["search-container"]}>
+              <img src="src/assets/search.png" alt="Cerca" onClick={()=>console.log("Cerca Cliccato")}/>
+              <input placeholder="Cerca" type="text"></input>
+            </div>
+            <select>
+              <option value="" disabled selected>Categoria</option>
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+            <p onClick={()=>console.log("Elimina filtri Cliccato")}>Elimina filtri</p>
+          </div>
+          <Table />
         </div>
     </div>
-);
+  );
 }
